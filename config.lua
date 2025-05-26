8f150606fae2b44d9fb02cdecdb88f5475270cdd
xDelito <89098263+xDelito@users.noreply.github.com>
2025-05-27 01:45:38 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+-- Prop model para la bolsa/drop en el mundo
+Config.ItemDropObject = `prop_ld_suitcase_01`
+
+-- Tiempo hasta limpieza automática de drops (minutos)
+Config.CleanupDropTime = 5
+Config.CleanupDropInterval = 1
+
+-- Distancia de interacción (metros)
+Config.InteractDistance = 2.0
+
+-- Blip settings
+Config.Blip = {
+    sprite = 351,
+    color = 2,
+    scale = 0.8,
+    text = "Tu Loot Bag"
+}
